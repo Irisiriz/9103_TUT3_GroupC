f9e92cc960eaeefdd4f10c0f9732fa1cd2c262ed
Irisiriz <yzha3003@uni.sydney.edu.au>
2025-06-09 22:41:52 +0800

    Final Group Code

---
+++ GroupCodeSketch.js
@@ -0,0 +1,100 @@
+// Member A qzha6629: Set colour palette, and canvas wide, height, background
+// Member B pluo0398: Yellow Grid
+// Member C xgao0847: Blocks
+// Member D yzha3003: Colour Dashes On Lines
+
+/**** Member A ****/
+let grid  = 20;
+let wide = 34 * grid;
+let height = wide;
+
+// Colour palette
+let colour = {
+  W : '#ffffff',
+  Y : '#f6e64b',
+  R : '#b33025',
+  B : '#2d59b5',
+  G : '#d8d8d8',
+};
+/**** Member A ****/
+
+/**** Member B ****/
+// Grid
+let vLines = [1, 3, 7, 12, 21, 29, 32];
+let hLines = [1, 5, 11, 13, 16, 19, 27, 32];
+/**** Member B ****/
+
+/**** Member C ****/
+// Blocks
+let blocks = [
+  {col: 1, row:  4, w: 1, h: 1, colour: colour.G},
+  {col: 1, row: 10, w: 3, h: 3, colour: colour.R},
+  {col: 1, row: 26, w: 3, h: 3, colour: colour.R},
+  {col: 5, row: 22, w: 1, h: 1, colour: colour.G},
+  
+  {col: 9, row:  1, w: 1, h: 1, colour: colour.G},
+  {col: 10, row:  4, w: 1, h: 1, colour: colour.R},
+  {col: 11, row:  7, w: 3, h: 6, colour: colour.B},
+  {col: 11, row:  9, w: 1, h: 2, colour: colour.R},
+  {col: 11, row: 15, w: 1, h: 1, colour: colour.G},
+  
+  {col: 11, row: 22, w: 3, h: 3, colour: colour.R},
+  {col: 11, row: 28, w: 1, h: 1, colour: colour.G},
+  {col: 15, row: 28, w: 1, h: 1, colour: colour.B},
+];
+/**** Member C ****/
+
+/**** Member A ****/
+function setup() {
+  createCanvas(wide, height);
+  noLoop();
+}
+
+function draw() {
+  background(colour.W);
+  /**** Member A ****/
+
+  /**** Member B ****/
+  // Draw lines
+  fill(colour.Y);
+  vLines.forEach(c => rect(c * grid, 0, grid, height));
+  hLines.forEach(r => rect(0, r * grid, wide, grid));
+  /**** Member B ****/
+
+  /**** Member D ****/
+  // Draw Dots
+  colourDashesOnLines();
+  //**** Member D ****/
+
+  /**** Member C ****/
+  // Draw Blocks
+  blocks.forEach(b => {
+    fill(b.colour);
+    rect(b.col * grid, b.row * grid, b.w * grid, b.h * grid);
+  });
+}
+/**** Member C ****/
+
+/**** Member D ****/
+function colourDashesOnLines() {
+  let accent = [colour.Y, colour.R, colour.B, colour.G];
+
+  vLines.forEach((c, idx) => {
+    for (let r = 0; r < height/grid; r++) {
+      if (random() < 0.33) {
+        fill(accent[(r + idx) % accent.length]);
+        rect(c * grid, r * grid, grid, grid);
+      }
+    }
+  });
+  
+  hLines.forEach((r, idx) => {
+    for (let c = 0; c < wide/grid; c++) {
+      if (random() < 0.33) {
+        fill(accent[(c + idx + 2) % accent.length]);
+        rect(c * grid, r * grid, grid, grid);
+      }
+    }
+  });
+}
+/**** Member D ****/

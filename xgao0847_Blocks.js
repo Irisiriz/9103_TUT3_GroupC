4fd2706146cb6eeab1e0e84a30b9467198f0c018
Nicole0316 <xgao0847@uni.sydney.edu.au>
2025-06-09 23:41:36 +1000

    Update xgao0847_Blocks.js
    Add block data array (Nicole)

--- xgao0847_Blocks.js
+++ xgao0847_Blocks.js
@@ -9,3 +9,9 @@ let colour = {
     B: '#2d59b5',
     Y: '#f6e64b'
 };
+
+let blocks = [
+    {col: 9, row: 1, w: 1, h: 1, colour: colour.G},
+    {col: 10, row: 4, w: 1, h: 3, colour: colour.R},
+    {col: 11, row: 7, w: 3, h: 6, colour: colour.B}
+];

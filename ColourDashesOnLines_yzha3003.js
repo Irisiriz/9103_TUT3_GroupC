421458524309dd741d00f05e37e3324e926c40c4
Irisiriz <yzha3003@uni.sydney.edu.au>
2025-06-09 21:38:40 +0800

    Colour Dashes On Lines by yzha3003.js

---
+++ ColourDashesOnLines_yzha3003.js
@@ -0,0 +1,26 @@
+// Colour Dashes On Lines by yzha3003
+function draw() {
+    colourDashesOnLines();
+}
+
+function colourDashesOnLines() {
+    let accent = [colour.Y, colour.R, colour.B, colour.G];
+  
+    vLines.forEach((c, idx) => {
+      for (let r = 0; r < height/grid; r++) {
+        if (random() < 0.33) {
+          fill(accent[(r + idx) % accent.length]);
+          rect(c * grid, r * grid, grid, grid);
+        }
+      }
+    });
+    
+    hLines.forEach((r, idx) => {
+      for (let c = 0; c < wide/grid; c++) {
+        if (random() < 0.33) {
+          fill(accent[(c + idx + 2) % accent.length]);
+          rect(c * grid, r * grid, grid, grid);
+        }
+      }
+    });
+  }

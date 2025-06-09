63f499c4edd9a2701b231637a4e34541bf397935
Nicole0316 <xgao0847@uni.sydney.edu.au>
2025-06-09 23:59:56 +1000

    Update xgao0847_Blocks.js
    Add final block data and drawBlocks() function (Nicole)

--- xgao0847_Blocks.js
+++ xgao0847_Blocks.js
@@ -11,7 +11,25 @@ let colour = {
 };
 
 let blocks = [
-    {col: 9, row: 1, w: 1, h: 1, colour: colour.G},
-    {col: 10, row: 4, w: 1, h: 3, colour: colour.R},
-    {col: 11, row: 7, w: 3, h: 6, colour: colour.B}
+    {col: 1, row:  4, w: 1, h: 1, colour: colour.G},
+    {col: 1, row: 10, w: 3, h: 3, colour: colour.R},
+    {col: 1, row: 26, w: 3, h: 3, colour: colour.R},
+    {col: 5, row: 22, w: 1, h: 1, colour: colour.G},
+
+    {col: 9, row:  1, w: 1, h: 1, colour: colour.G},
+    {col: 10, row:  4, w: 1, h: 1, colour: colour.R},
+    {col: 11, row:  7, w: 3, h: 6, colour: colour.B},
+    {col: 11, row:  9, w: 1, h: 2, colour: colour.R},
+    {col: 11, row: 15, w: 1, h: 1, colour: colour.G},
+
+    {col: 11, row: 22, w: 3, h: 3, colour: colour.R},
+    {col: 11, row: 28, w: 1, h: 1, colour: colour.G},
+    {col: 15, row: 28, w: 1, h: 1, colour: colour.B}
 ];
+
+function drawBlocks() {
+    blocks.forEach(b => {
+    fill(b.colour);
+    rect(b.col * grid, b.row * grid, b.w * grid, b.h * grid);
+    });
+}

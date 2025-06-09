c453c4f120f8713c1730df0a9535b1e0b81b7fa7
Irisiriz <yzha3003@uni.sydney.edu.au>
2025-06-09 21:19:16 +0800

    Create yzha3003_ColourDashesOnLines.js

---
+++ yzha3003_ColourDashesOnLines.js
@@ -0,0 +1,24 @@
+// Draw Dots
+colourDashesOnLines();
+
+function colourDashesOnLines() {
+    let accent = [colour.Y, colour.R, colour.B, colour.G];
+  
+    vLines.forEach((c, idx) => {
+      for (let r = 0; r < height/grid; r++) {
+        if (random() < 0.33) {
+          fill(accent[(r + idx) % accent.length]);
+          rect(c * grid, r * grid, grid, grid);
+        }
+      }
+    });
+    
+    hLines.forEach((r, idx) => {
+      for (let c = 0; c < wide/grid; c++) {
+        if (random() < 0.33) {
+          fill(accent[(c + idx + 2) % accent.length]);
+          rect(c * grid, r * grid, grid, grid);
+        }
+      }
+    });
+  }
